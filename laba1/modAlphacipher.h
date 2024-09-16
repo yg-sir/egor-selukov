3cd49b79593b96fd11f488edefeca37a2e444b4c
yg-sir <egor_sov2005@mail.ru>
2024-09-16 20:03:33 +0300

    Update modAlphacipher.h

--- laba1/modAlphacipher.h
+++ laba1/modAlphacipher.h
@@ -1,1 +1,19 @@
-
+#pragma once
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+class modAlphaCipher
+{
+private:
+    wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    map<wchar_t, int> alphaNum;
+    vector<int> key;
+    vector<int> convert(const wstring& s);
+    wstring convert(const vector<int>& v);
+public:
+    modAlphaCipher() = delete;
+    modAlphaCipher(const wstring& skey);
+    wstring encrypt(const wstring& open_text);
+    wstring decrypt(const wstring& cipher_text);
+};
