0b167167396d7004fdf9c163435a1175944bd51c
yg-sir <egor_sov2005@mail.ru>
2024-09-16 20:20:16 +0300

    Update Cipher.cpp

--- laba1/laba1.1/Cipher.cpp
+++ laba1/laba1.1/Cipher.cpp
@@ -1,1 +1,46 @@
-
+#include <Cipher.h>
+Cipher::Cipher(int skey) { key = skey; }
+string Cipher ::encrypt(string& text)
+{
+    int k = 0;
+    int simvoli = text.size();
+    int stroki = ((simvoli - 1) / key) + 1;
+    char** tabl = new char*[stroki];
+    for(int i = 0; i < stroki; i++)
+        tabl[i] = new char[key];
+    for(int i = 0; i < stroki; i++)
+        for(int j = 0; j < key; j++) {
+            if(k < simvoli) {
+                tabl[i][j] = text[k];
+                k++;
+            }
+        }
+    k = 0;
+    for(int j = key - 1; j >= 0; j--)
+        for(int i = 0; i < stroki; i++) {
+            text[k] = tabl[i][j];
+            k++;
+        }
+    return text;
+}
+string Cipher ::decrypt(string& text)
+{
+    int k = 0;
+    int simvoli = text.size();
+    int stroki = ((simvoli - 1) / key) + 1;
+    char** tabl = new char*[stroki];
+    for(int i = 0; i < stroki; i++)
+        tabl[i] = new char[key];
+    for(int j = key - 1; j >= 0; j--)
+        for(int i = 0; i < stroki; i++) {
+            tabl[i][j] = text[k];
+            k++;
+        }
+    k = 0;
+    for(int i = 0; i < stroki; i++)
+        for(int j = 0; j < key; j++) {
+            text[k] = tabl[i][j];
+            k++;
+        }
+    return text;
+}
