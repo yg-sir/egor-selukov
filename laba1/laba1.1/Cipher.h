446e02a13a737b5e315d4dea1ea8fa3cef8cfcb1
yg-sir <egor_sov2005@mail.ru>
2024-09-16 20:08:19 +0300

    Update Cipher.h

--- laba1/laba1.1/Cipher.h
+++ laba1/laba1.1/Cipher.h
@@ -1,1 +1,14 @@
-
+#pragma once
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+class modShifr {
+    private:
+        int key;
+    public:
+        modShifr()=delete;
+        modShifr(int skey);
+        string encrypt(string &text);
+        string decrypt(string &text);
+};
